158db28edf5448c0f29a169f333fd7fd99aff6c0
Hemru Chamuditha <chamudhitha123@gmail.com>
2024-06-13 20:07:32 +0530

    ...

---
+++ src/java/com/mycompany/food_odaring/controller/servlet_complete_order_placing.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.mycompany.food_odaring.controller;
+
+import com.mycompany.food_odaring.dao.class_order;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class servlet_complete_order_placing extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+             String itemList = request.getParameter("itemList");
+             String cusId =  request.getParameter("cusId");
+             String totalPrice = request.getParameter("totalPrice");
+             String location = request.getParameter("location");
+             
+             if(completeOrder(itemList , cusId , totalPrice , location)){
+                    out.print(1);
+             }else{
+                    out.print(0);
+             }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    private boolean completeOrder(String itemList, String cusId, String totalPrice, String location) {
+        ArrayList<Object> list=new ArrayList<>();
+        
+        list.add(itemList);
+        list.add(cusId);
+        list.add(totalPrice);
+        list.add(location);
+        
+        boolean isEdit = new class_order().saveDataToOrderPlace(list);
+        return isEdit;
+    }
+
+}
